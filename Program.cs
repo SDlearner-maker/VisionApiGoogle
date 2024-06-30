8ec74529e738c4957ff857bce5c30d4439ea78a7
SDlearner-maker <swetadas39@gmail.com>
2024-06-30 07:34:01 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+
+namespace VisionApiDemo
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromUri("gs://cloud-samples-data/vision/using_curl/shanghai.jpeg");
+            var labels = client.DetectLabels(image);
+
+            Console.WriteLine("Labels (and confidence score):");
+            Console.WriteLine(new String('=', 30));
+
+            foreach (var label in labels)
+            {
+                Console.WriteLine($"{label.Description} ({(int)(label.Score * 100)}%)");
+            }
+        }
+    }
+}
